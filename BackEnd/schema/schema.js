2cfc11cc423f11564d040c51bc16edfcfc3002f6
prav23 <61222132+prav23@users.noreply.github.com>
2021-05-17 22:57:04 -0700

    backend schema update

--- BackEnd/schema/schema.js
+++ BackEnd/schema/schema.js
@@ -1,9 +1,9 @@
-// /* eslint-disable no-unused-vars */
-// /* eslint-disable no-underscore-dangle */
-// /* eslint-disable consistent-return */
-
 const graphql = require('graphql');
 const User = require('../dbSchema/User');
+const Profile = require('../dbSchema/Profile');
+const Group = require('../dbSchema/Group');
+const Expense = require('../dbSchema/Expense');
+const UserGroup = require('../dbSchema/UserGroup');
 
 const { userLogin } = require('../mutations/login');
 const { registerUser } = require('../mutations/register');
@@ -19,6 +19,7 @@ const {
   GraphQLID,
   GraphQLInt,
   GraphQLList,
+  GraphQLFloat,
   GraphQLNonNull,
 } = graphql;
 
@@ -32,6 +33,51 @@ const UserType = new GraphQLObjectType({
   }),
 });
 
+const ProfileType = new GraphQLObjectType({
+  name: 'Profile',
+  fields: () => ({
+    _id: { type: GraphQLID },
+    user: { type: GraphQLString },
+    image: { type: GraphQLString },
+    phonenumber: { type: GraphQLInt },
+    currency: { type: GraphQLString },
+    language: { type: GraphQLString },
+    timezone: { type: GraphQLString },
+  }),
+});
+
+const GroupType = new GraphQLObjectType({
+  name: 'Group',
+  fields: () => ({
+    _id: { type: GraphQLID },
+    group_name: { type: GraphQLString },
+    group_image : { type: GraphQLString },
+  }),
+});
+
+const ExpenseType = new GraphQLObjectType({
+  name: 'Expense',
+  fields: () => ({
+    _id: { type: GraphQLID },
+    user: { type: GraphQLString },
+    group: { type: GraphQLString },
+    amount: { type: GraphQLInt },
+    description: { type: GraphQLString },
+    expense_date: { type: GraphQLString },
+  }),
+});
+
+const UserGroupType = new GraphQLObjectType({
+  name: 'UserGroup',
+  fields: () => ({
+    _id: { type: GraphQLID },
+    user: { type: GraphQLString },
+    group: { type: GraphQLString },
+    status: { type: GraphQLString },
+    total_spent : { type: GraphQLInt },
+    total_owed : { type: GraphQLInt },
+  }),
+});
 
 const StatusType = new GraphQLObjectType({
   name: 'Status',
